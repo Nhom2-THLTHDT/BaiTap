ad92b2b43a6dfdd98fb72203001336e1ff2f9782
ThaiNguyen <Catraconnguyen76@gmail.com>
2025-09-11 08:10:31 +0700

    Baitapnhom2

---
+++ Bai2.java
@@ -0,0 +1,15 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String ten = "Dang Thai Nguyen";
+        int tuoi = 19;
+        double chieuCao = 1.70;
+        char gioiTinh = 'M';
+        boolean cothichlaptrinhkhong = true;
+
+        System.out.println("Tên: " + ten);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("Chiều cao: " + chieuCao + " m");
+        System.out.println("Giới tính: " + gioiTinh);
+        System.out.println("Có thích lập trình không?:" + cothichlaptrinhkhong);
+    }
+}
